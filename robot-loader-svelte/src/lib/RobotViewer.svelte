c2796da8043a0a40a351f910ab33ecab51d019eb
limjiyoon <luinpark@gmail.com>
2024-04-28 00:18:21 +0900

    FEATURE: load robot

--- robot-loader-svelte/src/lib/RobotViewer.svelte
+++ robot-loader-svelte/src/lib/RobotViewer.svelte
@@ -1,28 +1,49 @@
 <script lang="ts">
     import * as THREE from 'three';
+    import {LoadingManager, MathUtils, Mesh} from 'three';
+    import URDFLoader from 'urdf-loader';
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    const ambientLight = new THREE.AmbientLight( 0xffffff ); // soft white light
+    scene.add( ambientLight );
 
-    camera.position.z = 5;
+    const size = 10;
+    const divisions = 10;
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add(gridHelper)
+
+    const ground = new Mesh(new THREE.PlaneGeometry(), new THREE.ShadowMaterial({ opacity: 0.25 }));
+    ground.rotation.x = -Math.PI / 2;
+    ground.scale.setScalar(30);
+    ground.receiveShadow = true;
+    scene.add(ground);
+
+    camera.position.y = 20;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 4;
+    controls.target.y = 1;
+    controls.update();
 
     function animate() {
-        requestAnimationFrame( animate );
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
-        renderer.render( scene, camera );
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
     }
-
     animate();
+
+    const manager = new LoadingManager();
+    const loader = new URDFLoader( manager );
+    loader.load(
+        '/src/assets/T12/urdf/T12.urdf',
+        (robot) => {
+            robot.rotation.x = -Math.PI / 2;
+            scene.add(robot);
+        }
+    )
 </script>
