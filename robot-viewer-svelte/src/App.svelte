99fbd0ccb0dd1a41ff9f75e21bc4faa4f7b409ca
limjiyoon <luinpark@gmail.com>
2024-05-06 10:40:12 +0900

    REFACTOR: re-organize code structure

--- robot-viewer-svelte/src/App.svelte
+++ robot-viewer-svelte/src/App.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
   import RobotViewer from './components/RobotViewer.svelte'
+
+  import URDFLoader, {type URDFRobot} from "urdf-loader";
+  import {LoaderUtils, LoadingManager} from "three";
+  import JointSliders from "./components/JointSliders.svelte";
+  async function getRobot(robotName: string): Promise<URDFRobot> {
+      const manager = new LoadingManager();
+      const loader = new URDFLoader(manager);
+      const getRobotUrdfURL = `http://localhost:8080/urdfs/${robotName}.urdf`;
+      loader.workingPath = LoaderUtils.extractUrlBase(getRobotUrdfURL);
+
+      let res = await fetch(getRobotUrdfURL);
+      let content: string = await res.text();
+      return loader.parse(content);
+  }
+
+  const robotPromise = getRobot("T12");
 </script>
 
 <main>
-    <RobotViewer />
+    <div id="controls">
+        <div id="joint-sliders">
+            <ul>
+            </ul>
+        </div>
+    </div>
+
+    {#await robotPromise}
+        <p>Waiting...</p>
+    {:then robot}
+        <JointSliders robot={robot}/>
+        <canvas>
+            <RobotViewer robot={robot}/>
+        </canvas>
+    {/await}
+
 </main>
--- robot-viewer-svelte/src/components/RobotViewer.svelte
+++ robot-viewer-svelte/src/components/RobotViewer.svelte
@@ -1,39 +1,64 @@
 <script lang="ts">
     import * as THREE from 'three';
-    import { LoaderUtils, LoadingManager } from 'three';
-    import RadianButton from "./RadianButton.svelte";
-    import URDFLoader, {type URDFRobot} from "urdf-loader";
+    import {DirectionalLight, Mesh, WebGLRenderer} from 'three';
+    import {type URDFRobot} from "urdf-loader";
     import DrawRobot from "./DrawRobot.svelte";
-    import Background from "./Background.svelte";
+    import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-    let camera: THREE.PerspectiveCamera;
-    let scene: THREE.Scene;
-    let renderer: THREE.WebGLRenderer;
+    export let robot: URDFRobot;
 
-    async function getRobot(robotName: string): Promise<URDFRobot> {
-        const manager = new LoadingManager();
-        const loader = new URDFLoader(manager);
-        const getRobotUrdfURL = `http://localhost:8080/urdfs/${robotName}.urdf`;
-        loader.workingPath = LoaderUtils.extractUrlBase(getRobotUrdfURL);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new WebGLRenderer({ antialias: true })!!;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild( renderer.domElement );
 
-        let res = await fetch(getRobotUrdfURL);
-        let content: string = await res.text();
-        return loader.parse(content);
+    const directionalLight = new DirectionalLight(0xffffff, 1.0);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.setScalar(1024);
+    directionalLight.position.set(5, 30, 5);
+    scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight( 0xffffff ); // soft white light
+    scene.add( ambientLight );
+
+    const size = 10;
+    const divisions = 10;
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add(gridHelper)
+
+    const ground = new Mesh(new THREE.PlaneGeometry(), new THREE.ShadowMaterial({ opacity: 0.25 }));
+    ground.rotation.x = -Math.PI / 2;
+    ground.scale.setScalar(30);
+    ground.receiveShadow = true;
+    scene.add(ground);
+
+    camera.position.y = 20;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 4;
+    controls.target.y = 1;
+    controls.update();
+
+    const onResize = () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+    };
+
+    const render = () => {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
     }
 
-    const robotPromise = getRobot("T12");
+    render();
+    onResize();
+    window.addEventListener('resize', onResize);
+
 </script>
 
-<div id="controls">
-    <RadianButton />
-    <div id="joint-controls">
-        <ul></ul>
-    </div>
-    <div id="robot-viewer"></div>
-</div>
-{#await robotPromise}
-    <p>Waiting...</p>
-{:then robot}
-    <Background scene={scene} camera={camera} renderer={renderer}/>
-    <DrawRobot robot={robot} scene={scene}/>
-{/await}
+<DrawRobot robot={robot} scene={scene}/>
